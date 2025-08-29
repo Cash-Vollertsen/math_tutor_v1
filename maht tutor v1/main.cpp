e3985d01ed1a84e15cc2869fc43e0270af47d20f
Cash Vollertsen <cash.vollertsen@gmail.com>
2025-08-29 09:40:03 -0500

    Update main.cpp

--- maht tutor v1/main.cpp
+++ maht tutor v1/main.cpp
@@ -5,8 +5,7 @@
     Date..........:9/5/25
     Description...:This will be the first iteration of our simple silly math tutor. It will be a very simple
                    version of a math tutor starting off but eventually growing into something that is very complex and
-                   something that we can be proud of. hello this is me from the other sided
-                   
+                   something that we can be proud of. 
     **********************************************/
 
 #include <iostream> // inlcude required for cin and cout
